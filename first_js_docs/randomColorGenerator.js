5f934b8cd9d917df934c8068ce414c6017080cde
Amit <amitkatz01@gmail.com>
2023-09-04 20:29:45 +0300

    h1 will change to random colors when hovered over

--- first_js_docs/randomColorGenerator.js
+++ first_js_docs/randomColorGenerator.js
@@ -2,6 +2,8 @@ document.querySelector("div").style.textAlign = "center";
 let bodyEl = document.querySelector("body");
 let btn = document.querySelector("button");
 let rgbLabel = document.querySelector("h2");
+let h1RandomColor = document.querySelector("h1"); //saving h1
+
 function ifClicked (){
 const red = Math.floor(Math.random() * 256);
 const green = Math.floor(Math.random() * 256);
@@ -10,5 +12,14 @@ const blue = Math.floor(Math.random() * 256);
 bodyEl.style.backgroundColor = `rgb(${red},${green},${blue})`;
 rgbLabel.innerText = `rgb(${red},${green},${blue})`
 }
+function ifHovered (){
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    //Generating rgb
+    h1RandomColor.style.color = `rgb(${red},${green},${blue})`;
+}
 btn.style.border = "0.15rem solid #a2d2ff";
 btn.addEventListener("click", ifClicked);
+h1RandomColor.addEventListener('mouseover', ifHovered);
+//h1 will change to random colors when hovered over
